a6c98d0684756e79bfced960bf1af842429a07ae
Ivan Botero <ivan.botero@protonmail.ch>
2017-04-30 16:42:06 -0500

    Se hace interfaz principal - Se agregan script y elementos para interfaz - Se agregan las clases y las interfaces -> Por el momento no hay ninguna funcionalidad

---
+++ index.php
@@ -0,0 +1,56 @@
+<html>
+    <head>
+        <title>
+            Gimnasio - Musculosos & Musculosas
+        </title>
+        <link rel="stylesheet" type="text/css" href="css/design.css">
+        <script type="text/javascript" src="js/jquery.js"></script>
+    </head>
+    <body>
+
+        <!--Contenedor-->
+        <div id="contenedor">
+
+            <!--Panel Izquierdo-->
+            <div id="panel_izquierdo">
+
+                <div>
+                    <input type="button" id="btn_registrar_cliente" class="boton_menu_principal" value="Registrar Cliente">
+                </div>
+                <div>
+                    <input type="button" id="btn_registrar_medidas" class="boton_menu_principal" value="Registrar Medidas">
+                </div>
+                <div>
+                    <input type="button" id="btn_registrar_servicio" class="boton_menu_principal" value="Registrar Servicio">
+                </div>
+
+            </div>
+
+            <!--Panel Central-->
+            <div id="panel_central">
+
+                <div>
+                    <input type="button" id="btn_imprimir_rutina" class="boton_menu_principal" value="Imprimir Rutina">
+                </div>
+
+            </div>
+
+            <!--Panel Derecho-->
+            <div id="panel_derecho">
+
+                <div>
+                    <input type="button" id="btn_registrar_rutina" class="boton_menu_principal" value="Registrar Rutina">
+                </div>
+                <div>
+                    <input type="button" id="btn_modificar_rutina" class="boton_menu_principal" value="Modificar Rutina">
+                </div>
+                <div>
+                    <input type="button" id="btn_consultar_cliente" class="boton_menu_principal" value="Consultar Cliente">
+                </div>
+
+            </div>
+
+        </div>
+
+    </body>
+</html>
