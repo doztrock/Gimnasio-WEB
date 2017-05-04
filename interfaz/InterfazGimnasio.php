dec1236ebb3b5aa4c1113397e4e40633089d05d8
Ivan Botero <ivan.botero@protonmail.ch>
2017-05-04 07:06:13 -0500

    Agregada estructura basica para cada elemento de interfaz

--- interfaz/InterfazGimnasio.php
+++ interfaz/InterfazGimnasio.php
@@ -5,7 +5,7 @@
 ?>
 <html>
     <head>
-        <script>
+        <script type="text/javascript">
 
             $(document).ready(function () {
 
@@ -15,30 +15,37 @@
 
                 /* Boton: Registrar Cliente */
                 $("#btn_registrar_cliente").click(function () {
+                    $("#contenedor").load("interfaz/InterfazRegistroCliente.php");
                 });
 
                 /* Boton: Registrar Medidas */
                 $("#btn_registrar_medidas").click(function () {
+                    $("#contenedor").load("interfaz/InterfazRegistroMedidas.php");
                 });
 
                 /* Boton: Registrar Servicio */
                 $("#btn_registrar_servicio").click(function () {
+                    $("#contenedor").load("interfaz/InterfazRegistroServicio.php");
                 });
 
                 /* Boton: Imprimir Rutina */
                 $("#btn_imprimir_rutina").click(function () {
+                    $("#contenedor").load("interfaz/InterfazImpresionRutina.php");
                 });
 
                 /* Boton: Registrar Rutina */
                 $("#btn_registrar_rutina").click(function () {
+                    $("#contenedor").load("interfaz/InterfazRegistroRutina.php");
                 });
 
                 /* Boton: Modificar Rutina */
                 $("#btn_modificar_rutina").click(function () {
+                    $("#contenedor").load("interfaz/InterfazModificacionRutina.php");
                 });
 
                 /* Boton: Consultar Cliente */
                 $("#btn_consultar_cliente").click(function () {
+                    $("#contenedor").load("interfaz/InterfazConsultaCliente.php");
                 });
 
             });
