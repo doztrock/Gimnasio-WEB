6b5078b3df4e78ca1cdaa9ee429640da6d53dc70
Ivan Botero <ivan.botero@protonmail.ch>
2017-05-04 07:25:46 -0500

    Agregado formulario de cliente sin diseno

--- interfaz/InterfazRegistroCliente.php
+++ interfaz/InterfazRegistroCliente.php
@@ -10,5 +10,67 @@
 	</head>
 	<body>
 	
+		<form>
+			
+			<div>
+
+				<div>
+					<label>Cedula</label>
+					<input type="text" id="input_cedula" name="input_cedula">
+				</div>
+
+				<div>
+					<label>Nombre:</label>
+					<input type="text" id="input_nombre" name="input_nombre">
+				</div>
+
+				<div>
+					<label>Genero:</label>
+					<select id="combo_genero" name="combo_genero">
+						<option value="M">Masculino</option>
+						<option value="F">Femenino</option>
+					</select>					
+				</div>
+
+				<div>
+					<label>Tipo de Sangre:</label>
+					<select id="combo_tipo_sangre" name="combo_tipo_sangre">
+						<option value="A+">A+</option>
+						<option value="A-">A-</option>
+						<option value="B+">B+</option>
+						<option value="B-">B-</option>
+						<option value="AB+">AB+</option>
+						<option value="AB-">AB-</option>
+						<option value="O+">O+</option>
+						<option value="O-">O-</option>
+					</select>
+				</div>
+
+				<div>
+					<label>Edad:</label>
+					<input type="text" id="input_edad" name="input_edad">
+				</div>
+
+				<div>
+					<label>Telefono:</label>
+					<input type="text" id="input_telefono" name="input_telefono">
+				</div>
+
+				<div>
+					<label>Direccion:</label>
+					<input type="text" id="input_direccion" name="input_direccion">
+				</div>
+
+				<div>
+					<label>EPS:</label>
+					<input type="text" id="input_eps" name="input_eps">
+				</div>
+
+			</div>
+
+			<input type="button" id="input_" name="input_" value="Registrar">
+
+		</form>
+
 	</body>
 </html>
