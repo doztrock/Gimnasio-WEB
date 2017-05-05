ae16c3f4c32e3ee37587fd99c38a8ce61515d3c8
Ivan Botero <ivan.botero@protonmail.ch>
2017-05-05 16:38:55 +0000

    Correcciones en menu principal

--- interfaz/InterfazGimnasio.php
+++ interfaz/InterfazGimnasio.php
@@ -2,6 +2,7 @@
 /**
  * Este es el menu principal de la aplicacion 
  */
+
 ?>
 <html>
     <head>
@@ -57,13 +58,13 @@
         <!--Panel Izquierdo-->
         <div id="panel_izquierdo">
 
-            <div>
+            <div class="contenedor_boton">
                 <input type="button" id="btn_registrar_cliente" class="boton_menu_principal" value="Registrar Cliente">
             </div>
-            <div>
+            <div class="contenedor_boton">
                 <input type="button" id="btn_registrar_medidas" class="boton_menu_principal" value="Registrar Medidas">
             </div>
-            <div>
+            <div class="contenedor_boton">
                 <input type="button" id="btn_registrar_servicio" class="boton_menu_principal" value="Registrar Servicio">
             </div>
 
@@ -72,7 +73,7 @@
         <!--Panel Central-->
         <div id="panel_central">
 
-            <div>
+            <div class="contenedor_boton">
                 <input type="button" id="btn_imprimir_rutina" class="boton_menu_principal" value="Imprimir Rutina">
             </div>
 
@@ -81,13 +82,13 @@
         <!--Panel Derecho-->
         <div id="panel_derecho">
 
-            <div>
+            <div class="contenedor_boton">
                 <input type="button" id="btn_registrar_rutina" class="boton_menu_principal" value="Registrar Rutina">
             </div>
-            <div>
+            <div class="contenedor_boton">
                 <input type="button" id="btn_modificar_rutina" class="boton_menu_principal" value="Modificar Rutina">
             </div>
-            <div>
+            <div class="contenedor_boton">
                 <input type="button" id="btn_consultar_cliente" class="boton_menu_principal" value="Consultar Cliente">
             </div>
 
