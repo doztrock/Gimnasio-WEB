c4d0e2a1e9c99b039463f7def052323028aa35d6
Ivan Botero <ivan.botero@protonmail.ch>
2017-05-07 22:39:32 -0500

    Diseno en registrar cliente hecho

--- interfaz/InterfazRegistroCliente.php
+++ interfaz/InterfazRegistroCliente.php
@@ -50,55 +50,87 @@
                 <div>
 
                     <div>
-                        <label class="label_formulario">Cedula:</label>
-                        <input type="text" class="input_formulario" id="input_cedula" name="input_cedula">
+                        <div class="caja_label_formulario">
+                            <label class="label_formulario">Cedula:</label>
+                        </div>
+                        <div class="caja_input_formulario">
+                            <input type="text" class="input_formulario" id="input_cedula" name="input_cedula">
+                        </div>
                     </div>
 
                     <div>
-                        <label class="label_formulario">Nombre:</label>
-                        <input type="text" class="input_formulario" id="input_nombre" name="input_nombre">
+                        <div class="caja_label_formulario">
+                            <label class="label_formulario">Nombre:</label>
+                        </div>
+                        <div class="caja_input_formulario">
+                            <input type="text" class="input_formulario" id="input_nombre" name="input_nombre">
+                        </div>
                     </div>
 
                     <div>
-                        <label class="label_formulario">Genero:</label>
-                        <select id="combo_genero" class="select_formulario" name="combo_genero">
-                            <option value="M">Masculino</option>
-                            <option value="F">Femenino</option>
-                        </select>					
+                        <div class="caja_label_formulario">
+                            <label class="label_formulario">Genero:</label>
+                        </div>
+                        <div class="caja_input_formulario">
+                            <select id="combo_genero" class="select_formulario" name="combo_genero">
+                                <option value="M">Masculino</option>
+                                <option value="F">Femenino</option>
+                            </select>
+                        </div>
                     </div>
 
                     <div>
-                        <label class="label_formulario">Tipo de Sangre:</label>
-                        <select id="combo_tipo_sangre" class="select_formulario" name="combo_tipo_sangre">
-                            <option value="A+">A+</option>
-                            <option value="A-">A-</option>
-                            <option value="B+">B+</option>
-                            <option value="B-">B-</option>
-                            <option value="AB+">AB+</option>
-                            <option value="AB-">AB-</option>
-                            <option value="O+">O+</option>
-                            <option value="O-">O-</option>
-                        </select>
+                        <div class="caja_label_formulario">
+                            <label class="label_formulario">Tipo de Sangre:</label>
+                        </div>
+                        <div class="caja_input_formulario">
+                            <select id="combo_tipo_sangre" class="select_formulario" name="combo_tipo_sangre">
+                                <option value="A+">A+</option>
+                                <option value="A-">A-</option>
+                                <option value="B+">B+</option>
+                                <option value="B-">B-</option>
+                                <option value="AB+">AB+</option>
+                                <option value="AB-">AB-</option>
+                                <option value="O+">O+</option>
+                                <option value="O-">O-</option>
+                            </select>
+                        </div>
                     </div>
 
                     <div>
-                        <label class="label_formulario">Edad:</label>
-                        <input type="text" class="input_formulario" id="input_edad" name="input_edad">
+                        <div class="caja_label_formulario">
+                            <label class="label_formulario">Edad:</label>
+                        </div>
+                        <div class="caja_input_formulario">
+                            <input type="text" class="input_formulario" id="input_edad" name="input_edad">
+                        </div>
                     </div>
 
                     <div>
-                        <label class="label_formulario">Telefono:</label>
-                        <input type="text" class="input_formulario" id="input_telefono" name="input_telefono">
+                        <div class="caja_label_formulario">
+                            <label class="label_formulario">Telefono:</label>
+                        </div>
+                        <div class="caja_input_formulario">
+                            <input type="text" class="input_formulario" id="input_telefono" name="input_telefono">
+                        </div>
                     </div>
 
                     <div>
-                        <label class="label_formulario">Direccion:</label>
-                        <input type="text" class="input_formulario" id="input_direccion" name="input_direccion">
+                        <div class="caja_label_formulario">
+                            <label class="label_formulario">Direccion:</label>
+                        </div>
+                        <div class="caja_input_formulario">
+                            <input type="text" class="input_formulario" id="input_direccion" name="input_direccion">
+                        </div>
                     </div>
 
                     <div>
-                        <label class="label_formulario">EPS:</label>
-                        <input type="text" class="input_formulario" id="input_eps" name="input_eps">
+                        <div class="caja_label_formulario">
+                            <label class="label_formulario">EPS:</label>
+                        </div>
+                        <div class="caja_input_formulario">
+                            <input type="text" class="input_formulario" id="input_eps" name="input_eps">
+                        </div>
                     </div>
 
                 </div>
